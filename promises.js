acb16e5a4c1d921562d8f60b7a7c165a27d7f08e
Akashkumar888 <akashgkr12@gmail.com>
2025-03-09 00:23:12 +0530

    update

--- promises.js
+++ promises.js
@@ -1,1 +1,52 @@
 
+
+function connectToServer(cbfn){
+  console.log("connecting to server...")
+  return new Promise(function(resolve,reject){
+    setTimeout(function(){
+      resolve("Server connected...")
+    })
+  },2000)
+}
+
+
+
+function getCourse(){
+  console.log("getting courses...");
+  return new Promise(function(resolve,reject){
+    setTimeout(function(){
+      resolve(["course :1","course :2","course :3"]);
+    },2000)
+  })
+}
+
+function fetchCourses(){
+  console.log("fetching courses...");
+  return new Promise(function(resolve,reject){
+    setTimeout(function(){
+      resolve(["course 1:DSA","course 2:Web dev"]);
+    })
+  })
+}
+
+connectToServer()
+
+.then(function(response){
+  console.log(response);
+
+  return getCourse();
+
+})
+.then(function(response){
+  console.log(response);
+  return fetchCourses();
+})
+.then(function(response){
+  console.log(response);
+})
+
+
+
+
+
+
