a37609c181ee97a2e0bb104ecac5f63f31eaf46a
Akashkumar888 <akashgkr12@gmail.com>
2025-04-07 03:22:13 +0530

    update

---
+++ form_rendering/post.js
@@ -0,0 +1,27 @@
+
+
+
+const express=require('express');
+const app=express();
+
+app.set('view engine','ejs');
+
+app.use(express.json());
+app.use(express.urlencoded({
+  extended:true
+}));
+
+app.get('/',function(req,res){
+  res.render("indexpost");
+})
+
+app.post('/check',function(req,res){
+  console.log(req.body);
+  res.send("Working...");
+})
+
+app.listen(3000);
+
+
+
+
