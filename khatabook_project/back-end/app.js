ee9cf46be58ed129d87fccd58e7e6e7e1ddcad5e
Akashkumar888 <akashgkr12@gmail.com>
2025-05-23 18:26:48 +0530

    update

--- khatabook_project/back-end/app.js
+++ khatabook_project/back-end/app.js
@@ -1,74 +1,90 @@
 
+
+
 const express=require('express');
-const path=require('path');
 const app=express();
+const path=require('path');
 const fs=require('fs');
-const { send } = require('process');
 
 
-app.set("view engine","ejs");
-app.use(express.static(path.join(__dirname,"public")));
+app.set("view engine",'ejs');
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname,"public")));
 
 
 
+app.get("/", function(req, res, next) {
+  fs.readdir("./hisaab", function(err, data) {
+    if (err) return res.status(500).send(err); // fixed: remove extra res
+    res.render("index", { files: data }); // pass correct variable
+  });
+});
 
 
-app.get('/',function(req,res,next){
-  res.send("Hey");
-})
-
-
-app.get('/create',(req,res,next)=>{
-  const currentDate=new Date();
-  const Day=String(currentDate.getDate()).padStart(2,'0');
-  const month=String(currentDate.getMonth()+1).padStart(2,'0');
-  const year=currentDate.getFullYear();
-  const fn=`${Day}-${month}-${year}.txt`;
-  fs.writeFile(`./files/${fn}`,"Daal Cheeni", function(err){
-  if(err)return res.send("Somethings went wrong...");
-  else res.send("Done...");
-  })
-})
-
-app.get('/files',function(req,res,next){
-  const fn=fs.readdir(`./files`,function(err,files){
-    res.render("index",{files});
-
-  })
+app.get('/create',function(req,res,next){
+  res.render("create");
 })
 
 
 app.get("/edit/:filename",function(req,res,next){
-  fs.readFile(`./files/${req.params.filename}`,"utf-8",function(err,data){
-    if(err) return res.send(err);
-    return res.render("edit",{data,filename: req.params.filename});
+  fs.readFile(`./hisaab/${req.params.filename}`,'utf-8',function(err,filedata){
+    if(err)return res.status(500).send(err);
+    res.render("edit",{filedata,filename : req.params.filename});
   })
 })
 
 
 app.post("/update/:filename",function(req,res,next){
-  fs.writeFile(`./files/${req.params.filename}`, req.body.filedata,function(err){
-    if(err) return res.send(err);
-    return res.redirect("/");
-  })
-})
-
-
-app.get("/delete/:filename",function(req,res,next){
-  fs.unlink(`./files/${req.params.filename}`,function(err){
-    if(err) return res.send(err);
-    return res.redirect("/");
-  })
+   fs.writeFile(`./hisaab/${req.params.filename}`,req.body.content,function(err){
+    if(err)return res.status(500).send(err);
+    res.redirect("/");
+   });
 })
 
 
 
+app.get("/hisaab/:filename",function(req,res){
+  fs.readFile(`./hisaab/${req.params.filename}`,'utf-8',function(err,filedata){
+    if(err)return res.status(500).send(err);
+    res.render("hisaab",{filedata ,filename : req.params.filename});
+  })
+
+})
 
 
 
-app.listen(3000);
+app.get("/delete/:filename",function(req,res){
+  fs.unlink(`./hisaab/${req.params.filename}`,function(err){
+    if(err)return res.status(500).send(err);
+    res.redirect("/");
+  })
+})
+
+
+
+app.post("/createhisaab",function(req,res,next){
+  var currentDate=new Date();
+  var DateTime=`${currentDate.getDate()}-${currentDate.getMonth()+1}-${currentDate.getFullYear()}`;
+  // console.log(DateTime);
+  fs.writeFile(`./hisaab/${DateTime}.txt`,req.body.content,function(err){
+    if(err)return res.status(500).send(err);
+    res.redirect("/");
+  });
+
+});
+
+
+
+// app.listen(3000);
+
+app.listen(3000, () => {
+  console.log("Server is running on http://localhost:3000");
+});
+
+
+
+
 
 
 
